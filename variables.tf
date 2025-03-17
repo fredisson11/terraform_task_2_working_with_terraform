2d5e9b5da243149560738ca0cd8660536330caec
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-03-17 20:25:37 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type = string
+  default = "task2rg"
+}
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+variable "storage_account_name" {
+  type = string
+  default = "task2sa"
+}
+variable "container_name" {
+  type = string
+  default = "task2container"
+}
+variable "blob_name" {
+  type = string
+  default = "task2blob"
+}
